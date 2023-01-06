b86299965167467d5b79f1a54a0113a384bfd998
James Argyropoulos <jamesargy6@gmail.com>
2023-01-05 22:03:18 -0800

    task(): add chunking to batchWrite

--- data-storage/test/dynamoDB/client.test.ts
+++ data-storage/test/dynamoDB/client.test.ts
@@ -18,6 +18,31 @@ describe('Dynamoclient', () => {
     expect(thing._client).toBe(mockDynamoDBClient)
   })
 
+  test('_batchWrite', async () => {
+    const play_index = '2022-04-01:1:000'
+    const batter_id = 12345
+    const play = { }
+    const dynamoPlay = { batter_id, play_index, play }
+    
+    const expectedBatchWriteInput1 = {
+      RequestItems: {
+        'bts-play': Array(25).fill({ PutRequest: { Item: { batter_id, play_index, play } } })
+      }
+    }
+
+    const expectedBatchWriteInput2 = {
+      RequestItems: {
+        'bts-play': [{ PutRequest: { Item: { batter_id, play_index, play } } }]
+      }
+    }
+
+    const thing = makeThing()
+    await thing.writePlays(Array(26).fill(dynamoPlay))
+    expect(mockDynamoDBClient.batchWrite).toHaveBeenNthCalledWith(1, expectedBatchWriteInput1)
+    expect(mockDynamoDBClient.batchWrite).toHaveBeenNthCalledWith(2, expectedBatchWriteInput2)
+
+  })
+
   test('writePlays', async () => {
     const play_index = '2022-04-01:1:000'
     const batter_id = 12345
