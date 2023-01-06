b37eb9bed1436fb0469d1533357e47bf450c078d
James Argyropoulos <jamesargy6@gmail.com>
2023-01-05 22:03:18 -0800

    task(): add chunking to batchWrite

--- data-storage/src/dynamoDB/client.ts
+++ data-storage/src/dynamoDB/client.ts
@@ -4,20 +4,35 @@ import { DynamoPlay } from './types'
 
 const BTS_PLAY_TABLE_NAME = 'bts-play'
 
+const MAX_CHUNK_SIZE = 25
+
 class DynamoClient {
   _client: DynamoDBDocument
   constructor(client: DynamoDBDocument) {
     this._client = client
   }
 
-  async writePlays(dynamoPlays: Array<DynamoPlay>) {
-    const putRequests = dynamoPlays.map(dynamoPlay => ({ PutRequest: { Item: dynamoPlay } }))
-    const input = {
+  async _batchWrite(tableName: string, items: Array<object>) {
+    const chunkArray = (arr, size): Array<Array<object>> =>
+      arr.length > size
+        ? [arr.slice(0, size), ...chunkArray(arr.slice(size), size)]
+        : [arr]
+    
+    const requests = items.map(item => ({ PutRequest: { Item: item } }))
+    const requestChunks = chunkArray(requests, MAX_CHUNK_SIZE)
+    
+    const batchWriteInputs = requestChunks.map(requestChunk => ({
       RequestItems: {
-        [BTS_PLAY_TABLE_NAME]: putRequests
+        [tableName]: requestChunk
       }
-    }
-    await this._client.batchWrite(input)
+    }))
+
+    const batchWritePromises = batchWriteInputs.map(input => this._client.batchWrite(input))
+    await Promise.all(batchWritePromises)
+  }
+
+  async writePlays(dynamoPlays: Array<DynamoPlay>) {
+    await this._batchWrite(BTS_PLAY_TABLE_NAME, dynamoPlays)
   }
 }
 
