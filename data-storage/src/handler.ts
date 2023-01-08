e4c5589a0661fc83c6a3124432f02cf378867178
James Argyropoulos <jamesargy6@gmail.com>
2023-01-07 16:27:58 -0800

    task(): use gamePk instead of gameNumber for gameIndex

--- data-storage/src/handler.ts
+++ data-storage/src/handler.ts
@@ -6,13 +6,13 @@ import { DynamoPlay } from './dynamoDB/types'
 type WritePlaysToDynamoInput = {
   transactionId: string
   date: string,
-  gameNumber: number,
+  gamePk: number,
   plays: Array<{ batterId: number }>
 }
 const writePlaysToDynamo = async (event: WritePlaysToDynamoInput) => {
   const dynamoClient = makeDynamoClient<DynamoPlay>(BTS_PLAY_TABLE_NAME)
-  const { transactionId, date, gameNumber, plays } = event
-  const gameIndex = toGameIndex(date, gameNumber)
+  const { transactionId, date, gamePk, plays } = event
+  const gameIndex = toGameIndex(date, gamePk)
   const dynamoPlays = toDynamoPlays(transactionId, gameIndex, plays)
   await dynamoClient.batchWrite(dynamoPlays)
 }
