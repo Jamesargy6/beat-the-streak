8b5b1c4cc2c0d0385abf6ed6b50e8b38d0796a5f
James Argyropoulos <jamesargy6@gmail.com>
2023-01-15 20:58:45 -0800

    task(): add error files, fix compilation, fix action config

---
+++ data-scraper-app/src/mlbStatsAPI/errors.ts
@@ -0,0 +1,11 @@
+
+class GameNotFoundError extends Error {
+  __proto__ = Error
+  /* istanbul ignore next */
+  constructor(gamePk: number) {
+    super(`Game "${gamePk}" was not found.`)
+    Object.setPrototypeOf(this, GameNotFoundError.prototype)
+  }
+} 
+
+export { GameNotFoundError }
