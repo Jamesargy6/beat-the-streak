ea9dc342eeac7abcefed354b75a7264402168f39
James Argyropoulos <jamesargy6@gmail.com>
2023-03-18 19:05:39 -0700

    request only the fields we need from the API

--- data-scraper-app/src/mlbStatsAPI/client.ts
+++ data-scraper-app/src/mlbStatsAPI/client.ts
@@ -1,4 +1,4 @@
-import MLBStatsAPI, { HydrationOptions } from 'mlb-stats-api'
+import MLBStatsAPI, { ScheduleHydrationOptions, ScheduleFieldOptions, PlayByPlayFieldOptions } from 'mlb-stats-api'
 import { GameType, PlayByPlay, Schedule, SportID } from 'mlb-stats-api'
 class MLBStatsAPIClient {
     _client: MLBStatsAPI
@@ -8,17 +8,39 @@ class MLBStatsAPIClient {
 
     async getRegularSeasonGames(startDate: string, endDate: string): Promise<Schedule> {
       const hydrationOptions = [
-        HydrationOptions.Lineups,
-        HydrationOptions.Venue,
-        HydrationOptions.Weather,
-        HydrationOptions.ProbablePitcher
+        ScheduleHydrationOptions.Lineups,
+        ScheduleHydrationOptions.Venue,
+        ScheduleHydrationOptions.Weather,
+        ScheduleHydrationOptions.ProbablePitcher
+      ]
+      const fieldsOptions = [
+        ScheduleFieldOptions.Dates,
+        ScheduleFieldOptions.Date,
+        ScheduleFieldOptions.Games,
+        ScheduleFieldOptions.GamePk,
+        ScheduleFieldOptions.OfficialDate,
+        ScheduleFieldOptions.GameNumber,
+        ScheduleFieldOptions.Venue,
+        ScheduleFieldOptions.Id,
+        ScheduleFieldOptions.Weather,
+        ScheduleFieldOptions.Condition,
+        ScheduleFieldOptions.Temp,
+        ScheduleFieldOptions.Wind,
+        ScheduleFieldOptions.Teams,
+        ScheduleFieldOptions.Away,
+        ScheduleFieldOptions.Home,
+        ScheduleFieldOptions.ProbablePitcher,
+        ScheduleFieldOptions.Lineups,
+        ScheduleFieldOptions.HomePlayers,
+        ScheduleFieldOptions.AwayPlayers
       ]
       const params = {
         sportId: SportID.MLB,
         startDate,
         endDate,
         gameType: GameType.RegularSeason,
-        hydrate: hydrationOptions.join(',')
+        hydrate: hydrationOptions.join(','),
+        fields: fieldsOptions.join(',')
       }
       const response = await this._client.getSchedule({ params })
       return response.data
@@ -26,7 +48,22 @@ class MLBStatsAPIClient {
 
     async getPlayByPlay(gamePk: number): Promise<PlayByPlay> {
       const pathParams = { gamePk }
-      const response = await this._client.getGamePlayByPlay({ pathParams })
+      const fieldsOptions = [
+        PlayByPlayFieldOptions.AllPlays,
+        PlayByPlayFieldOptions.Result,
+        PlayByPlayFieldOptions.EventType,
+        PlayByPlayFieldOptions.About,
+        PlayByPlayFieldOptions.IsComplete,
+        PlayByPlayFieldOptions.Matchup,
+        PlayByPlayFieldOptions.Batter,
+        PlayByPlayFieldOptions.Id,
+        PlayByPlayFieldOptions.BatSide,
+        PlayByPlayFieldOptions.Code,
+        PlayByPlayFieldOptions.Pitcher,
+        PlayByPlayFieldOptions.PitchHand
+      ]
+      const params = { fields: fieldsOptions.join(',') }
+      const response = await this._client.getGamePlayByPlay({ pathParams, params })
       return response.data
     }
 }
--- data-scraper-app/test/mlbStatsAPI/client.test.ts
+++ data-scraper-app/test/mlbStatsAPI/client.test.ts
@@ -1,4 +1,4 @@
-import { GameType, HydrationOptions, PlayByPlay, Schedule, SportID } from 'mlb-stats-api'
+import { GameType, ScheduleHydrationOptions, PlayByPlay, Schedule, SportID, ScheduleFieldOptions, PlayByPlayFieldOptions } from 'mlb-stats-api'
 import { MLBStatsAPIClient } from '../../src/mlbStatsAPI/client'
 
 describe('MLBStatsAPIClient', () => {
@@ -30,17 +30,39 @@ describe('MLBStatsAPIClient', () => {
     const startDate = '2022-01-31'
     const endDate = '2022-12-31'
     const hydrationOptions = [
-      HydrationOptions.Lineups,
-      HydrationOptions.Venue,
-      HydrationOptions.Weather,
-      HydrationOptions.ProbablePitcher
+      ScheduleHydrationOptions.Lineups,
+      ScheduleHydrationOptions.Venue,
+      ScheduleHydrationOptions.Weather,
+      ScheduleHydrationOptions.ProbablePitcher
+    ]
+    const fieldsOptions = [
+      ScheduleFieldOptions.Dates,
+      ScheduleFieldOptions.Date,
+      ScheduleFieldOptions.Games,
+      ScheduleFieldOptions.GamePk,
+      ScheduleFieldOptions.OfficialDate,
+      ScheduleFieldOptions.GameNumber,
+      ScheduleFieldOptions.Venue,
+      ScheduleFieldOptions.Id,
+      ScheduleFieldOptions.Weather,
+      ScheduleFieldOptions.Condition,
+      ScheduleFieldOptions.Temp,
+      ScheduleFieldOptions.Wind,
+      ScheduleFieldOptions.Teams,
+      ScheduleFieldOptions.Away,
+      ScheduleFieldOptions.Home,
+      ScheduleFieldOptions.ProbablePitcher,
+      ScheduleFieldOptions.Lineups,
+      ScheduleFieldOptions.HomePlayers,
+      ScheduleFieldOptions.AwayPlayers
     ]
     const expectedParams = {
       sportId: SportID.MLB,
       startDate,
       endDate,
       gameType: GameType.RegularSeason,
-      hydrate: hydrationOptions.join(',') 
+      hydrate: hydrationOptions.join(','),
+      fields: fieldsOptions.join(',')
     }
     
     test('happy path', async () => {
@@ -56,11 +78,26 @@ describe('MLBStatsAPIClient', () => {
     const expectedPathParams = {
       gamePk: testGamePk
     }
+    const fieldsOptions = [
+      PlayByPlayFieldOptions.AllPlays,
+      PlayByPlayFieldOptions.Result,
+      PlayByPlayFieldOptions.EventType,
+      PlayByPlayFieldOptions.About,
+      PlayByPlayFieldOptions.IsComplete,
+      PlayByPlayFieldOptions.Matchup,
+      PlayByPlayFieldOptions.Batter,
+      PlayByPlayFieldOptions.Id,
+      PlayByPlayFieldOptions.BatSide,
+      PlayByPlayFieldOptions.Code,
+      PlayByPlayFieldOptions.Pitcher,
+      PlayByPlayFieldOptions.PitchHand
+    ]
+    const expectedParams = { fields: fieldsOptions.join(',') }
     
     test('happy path', async () => {
       const thing = getThing()
       const result = await thing.getPlayByPlay(testGamePk)
-      expect(mockGetGamePlayByPlay).toHaveBeenCalledWith({ pathParams: expectedPathParams })
+      expect(mockGetGamePlayByPlay).toHaveBeenCalledWith({ pathParams: expectedPathParams, params: expectedParams })
       expect(result).toBe(testPlayByPlay)
     })
   })
